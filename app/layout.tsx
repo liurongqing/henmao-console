26adb6b02bcc0bfabe4e2e23cd96873866dd8b39
liurongqing <liurongqing@liurongqingdeiMac.local>
2023-05-08 00:27:49 +0800

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Layout, ConfigProvider } from "antd";
+import { StyleProvider } from "@ant-design/cssinjs";
 import zhCN from "antd/locale/zh_CN";
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
@@ -17,9 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="app">
         <ConfigProvider locale={zhCN}>
-          {/* <StyleProvider ssrInline={true}> */}
-          <Layout className="min-h-screen flex flex-col">{children}</Layout>
-          {/* </StyleProvider> */}
+          <StyleProvider hashPriority="high">
+            <Layout className="min-h-screen flex flex-col">{children}</Layout>
+          </StyleProvider>
         </ConfigProvider>
       </body>
     </html>
