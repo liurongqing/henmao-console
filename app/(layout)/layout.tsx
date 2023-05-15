4c8698c6dfe525893be0b73a6e53c5d450cb0ce2
rongqingliu <phpgege@gmail.com>
2023-05-15 19:55:59 +0800

    gohome

--- app/(layout)/layout.tsx
+++ app/(layout)/layout.tsx
@@ -1,18 +1,62 @@
 "use client";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, Dropdown } from "antd";
 import { ProLayout, PageContainer, ProCard } from "@ant-design/pro-components";
+import { AiOutlineLogout } from "react-icons/ai";
 import zhCN from "antd/locale/zh_CN";
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
+import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
 import { layoutDefaultProps } from "./layoutDefaultProps";
 
 dayjs.locale("en");
 
 const RootLayout = ({ children }) => {
   console.log("layout层 layout...");
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    console.log("退出登录");
+  };
+
+  console.log("pathname", pathname);
+  const layoutProps = {
+    location: { pathname },
+    menuItemRender: (item, dom) => <Link href={item.path}>{dom}</Link>,
+    avatarProps: {
+      src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
+      size: "small",
+      title: "一个男孩子",
+      render: (props, dom) => {
+        return (
+          <Dropdown
+            menu={{
+              items: [
+                {
+                  key: "logout",
+                  icon: <AiOutlineLogout />,
+                  label: "退出登录",
+                  onClick: handleLogout,
+                },
+              ],
+            }}
+          >
+            {dom}
+          </Dropdown>
+        );
+      },
+    },
+    onMenuHeaderClick: () => {
+      // 点击 logo，如果 pathname 不是 / 则，跳转到 /
+      if (pathname !== "/") {
+        router.replace("/");
+      }
+    },
+  };
   return (
     <ConfigProvider locale={zhCN}>
-      <ProLayout {...layoutDefaultProps}>
+      <ProLayout {...layoutDefaultProps} {...layoutProps}>
         <PageContainer fixedHeader>
           <ProCard className="min-h-[calc(100vh-120px)]">{children}</ProCard>
         </PageContainer>
--- app/(layout)/layoutDefaultProps.tsx
+++ app/(layout)/layoutDefaultProps.tsx
@@ -1,62 +1,30 @@
-import {
-  AiOutlineSmile,
-  AiOutlineCrown,
-  AiOutlineLogout,
-} from "react-icons/ai";
+import { AiFillDashboard, AiOutlineSetting } from "react-icons/ai";
+import { GiCat } from "react-icons/gi";
 import Link from "next/link";
-import { Dropdown } from "antd";
 
 export const layoutDefaultProps = {
   layout: "top",
   fixedHeader: true,
-  menuItemRender: (item, dom) => <Link href={item.path}>{dom}</Link>,
-  avatarProps: {
-    src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
-    size: "small",
-    title: "一个男孩子",
-    render: (props, dom) => {
-      return (
-        <Dropdown
-          menu={{
-            items: [
-              {
-                key: "logout",
-                icon: <AiOutlineLogout />,
-                label: "退出登录",
-                // onClick: handleLogout,
-              },
-            ],
-          }}
-        >
-          {dom}
-        </Dropdown>
-      );
-    },
-  },
+  // pageTitleRender: false,
+  title: "HenMao",
+  logo: <GiCat color="black" />,
+  // menuItemRender: (item, dom) => <Link href={item.path}>{dom}</Link>,
   route: {
-    path: "/",
+    // path: "/",
     routes: [
       {
         path: "/",
-        name: "欢迎",
-        icon: <AiOutlineSmile />,
+        name: "Dashboard",
+        icon: <AiFillDashboard />,
       },
       {
-        path: "/admin",
-        name: "管理页",
-        icon: <AiOutlineCrown />,
+        path: "/",
+        name: "游戏管理",
+        icon: <AiOutlineSetting />,
         routes: [
           {
-            path: "/admin/sub-page1",
-            name: "一级页面",
-          },
-          {
-            path: "/admin/sub-page2",
-            name: "二级页面",
-          },
-          {
-            path: "/admin/sub-page3",
-            name: "三级页面",
+            path: "/level",
+            name: "关卡列表",
           },
         ],
       },
