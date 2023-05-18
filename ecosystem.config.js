f7351cb8d24941b6d48d87c9d1e0d137be65fc45
liurongqing <liurongqing@liurongqingdeiMac.local>
2023-05-18 21:30:11 +0800

    1

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'henmao-console',
+      script: 'npm start',
+      watch: true,
+      ignore_watch: ['node_modules', 'log'],
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: '182.254.138.39',
+      ref: 'origin/main',
+      repo: 'git@github.com:liurongqing/henmao-console.git',
+      path: '/home/ubuntu/console',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
